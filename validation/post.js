5ef4ba92518ee89cae728c7a470457a305bb8d70
Dillon Titcomb <dillontitcomb@gmail.com>
2018-10-01 14:35:19 -0700

    add post route for creating posts.

---
+++ validation/post.js
@@ -0,0 +1,23 @@
+const Validator = require('validator');
+// Note: validator only works on strings
+const isEmpty = require('./is-empty');
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  // Ensure inputs are strings even if empty
+  data.text = !isEmpty(data.text) ? data.text : '';
+
+  if (!Validator.isLength(data.text, { min: 10, max: 300 })) {
+    errors.text = 'Post must be between 10 and 300 characters';
+  }
+
+  if (Validator.isEmpty(data.text)) {
+    errors.text = 'Text field is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
