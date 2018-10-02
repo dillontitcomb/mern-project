fef0fa9e741a7924308e1183e0a9486aa064106d
Dillon Titcomb <dillontitcomb@gmail.com>
2018-10-02 11:34:19 -0700

    add get all posts and get post by id requests.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -12,6 +12,25 @@ const validatePostInput = require('../../validation/post');
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Posts works' }));
 
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: 'No posts found' }));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404).json({ nopostfound: 'No post found with that ID' }));
+});
+
 // TODO: Change name and avatar to come from body rather than user
 // @route   POST api/posts
 // @desc    Create post
