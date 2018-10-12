ade9625edaf5a6a0883be024befdfe68c3013c0f
Dillon Titcomb <dillontitcomb@gmail.com>
2018-10-12 12:13:07 -0700

    add PrivateRoute component for conditional dashboard routing.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import jwt_decode from 'jwt-decode';
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { logoutUser, setCurrentUser } from './actions/authActions';
 import { clearCurrentProfile } from './actions/profileActions';
 import './App.css';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import PrivateRoute from './components/common/PrivateRoute';
 import Dashboard from './components/dashboard/Dashboard';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
@@ -46,7 +47,9 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
-              <Route exact path="/dashboard" component={Dashboard} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
             </div>
             <Footer />
           </div>
